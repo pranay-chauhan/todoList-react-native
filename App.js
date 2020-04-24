82999eac9f869da59109d5d944d44a13c0b2258a
pranay <pranay.s.chauhan@gmail.com>
2020-04-24 12:49:27 +0530

    my first commit in react-native-todo-app

--- App.js
+++ App.js
@@ -1,43 +1,96 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header';
+import InputBar from './components/InputBar';
+import TodoItem from './components/TodoItem';
 
-const Fruit =(props)=>{
-  return(
-    <View>
-      <Text>{props.name}</Text>
-  <Text>{props.cost}</Text>
-    </View>
-  )
-}
+export default class App extends React.Component {
+  constructor () {
+    super();
 
+    this.state = {
+      todoInput: '',
+      todos: [
+        { id: 0, title: 'Bring Vegetable', done: false },
+        { id: 1, title: 'Study React JS', done: false }
+      ]
+    }
+  }
 
-class App extends React.Component{
-  render(){
-    const Fruits = [
-      {name:'Apple',cost:400},
-      {name:'Mango',cost:700},
-      {name:'PineApple',cost:800}
-    ]
-    return(
+  addNewTodo () {
+    let todos = this.state.todos;
+
+    todos.unshift({
+      id: todos.length + 1,
+      title: this.state.todoInput,
+      done: false
+    });
+
+    this.setState({
+      todos: todos,
+      todoInput: ''
+    });
+  }
+
+  toggleDone (item) {
+    let todos = this.state.todos;
+
+    todos = todos.map((todo) => {
+      if (todo.id == item.id) {
+        todo.done = !todo.done;
+      }
+
+      return todo;
+    })
+
+    this.setState({todos});
+  }
+
+  removeTodo (item) {
+    let todos = this.state.todos;
+
+    todos = todos.filter((todo) => todo.id !== item.id);
+
+    this.setState({todos});
+  }
+
+  render() {
+    const statusbar = (Platform.OS == 'android') ? <View style={styles.statusbar}></View> : <View></View>;
+
+    return (
       <View style={styles.container}>
-         {
-           Fruits.map((fruit)=>{
-             return(
-               <Fruit name={fruit.name} cost={fruit.cost}/>
-             )
-           })
-         }
+        {statusbar}
+
+        <Header title="Todoapp" />
+
+        <InputBar
+          addNewTodo={() => this.addNewTodo()}
+          textChange={todoInput => this.setState({ todoInput })}
+          todoInput={this.state.todoInput}
+        />
+
+        <FlatList
+          data={this.state.todos}
+          extraData={this.state}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <TodoItem todoItem={item} toggleDone={() => this.toggleDone(item)} removeTodo={() => this.removeTodo(item)} />
+            )
+          }}
+        />
       </View>
-    )
+    );
   }
 }
-export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
+  statusbar: {
+    backgroundColor: '#FFCE00',
+    height: 20
+  }
 });
